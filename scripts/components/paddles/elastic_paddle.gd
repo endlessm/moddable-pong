81c066824f73e3a55fbf2f0bc250dda1591f4d15
Manuel Quiñones <manuel.por.aca@gmail.com>
2024-05-16 17:45:19 -0300

    Add initial assets

--- scripts/components/paddles/elastic_paddle.gd
+++ scripts/components/paddles/elastic_paddle.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 ## Is this the left paddle? Or the right one?
-@export var side: Global.Direction = Global.Direction.LEFT
+@export var player: Global.Player = Global.Player.LEFT
 
 ## How long can this paddle be.
 @export var max_length = 600.0
@@ -19,18 +19,23 @@ extends CharacterBody2D
 
 @onready var _initial_length = _shape.size.y
 
+@onready var _asset = %NinePatchRect
+@onready var _asset_initial_position_y = %NinePatchRect.position.y
+
 var _last_direction = 1
 
 
 func _physics_process(_delta):
 	var direction
-	if side == Global.Direction.RIGHT:
+	if player == Global.Player.RIGHT:
 		direction = Input.get_axis("ui_up", "ui_down")
 	else:
 		direction = Input.get_axis("player_2_up", "player_2_down")
 	if direction:
 		if _shape.size.y < max_length:
 			_shape.size.y += enlarge_speed
+			_asset.size.y += enlarge_speed
+			_asset.position.y -= enlarge_speed / 2
 			velocity.y = direction * (speed - enlarge_speed)
 		else:
 			velocity.y = direction * speed
@@ -41,5 +46,7 @@ func _physics_process(_delta):
 		else:
 			velocity.y = move_toward(velocity.y, 0, speed)
 		_shape.size.y = move_toward(_shape.size.y, _initial_length, shrink_speed)
+		_asset.size.y = move_toward(%NinePatchRect.size.y, _initial_length, shrink_speed)
+		_asset.position.y = move_toward(%NinePatchRect.position.y, _asset_initial_position_y, shrink_speed)
 
 	move_and_slide()
