ba755e8da1ef7d8250c3f8077362fd7dc39fd357
Manuel Quiñones <manuel.por.aca@gmail.com>
2024-05-16 11:16:26 -0300

    Remove unintended print

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -8,6 +8,5 @@ extends CanvasLayer
 
 ## Sets the score for each player:
 func set_players_scores(left_score, right_score):
-	prints(left_score, right_score)
 	_score_labels[Global.Player.LEFT].text = str(left_score)
 	_score_labels[Global.Player.RIGHT].text = str(right_score)
