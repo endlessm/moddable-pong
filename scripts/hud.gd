521789907d4def8a4f9cf781647a6ef35ecb0393
Manuel Quiñones <manuel.por.aca@gmail.com>
2024-05-17 12:38:17 -0300

    HUD: Allow changing the color

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,9 +1,23 @@
+@tool
 extends CanvasLayer
+
+## The color of the scores and middle line.
+@export var color: Color = Color.WHITE:
+	set = _set_color
 
 @onready var _score_labels = {
 	Global.Player.LEFT: %PlayerLeftScore,
 	Global.Player.RIGHT: %PlayerRightScore,
 }
+
+
+func _set_color(new_color):
+	if not Engine.is_editor_hint():
+		await ready
+	color = new_color
+	%Lines.modulate = color
+	for label in _score_labels.values():
+		label.add_theme_color_override("font_color", color)
 
 
 ## Sets the score for each player:
