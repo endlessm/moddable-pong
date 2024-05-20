7e24d322082f2781ea8565dd54e664b1628d6c38
Manuel Quiñones <manuel.por.aca@gmail.com>
2024-05-20 14:34:18 -0300

    Expose ball size
    If the instantiated ball is scaled directly, the editor shows a warning
    "Size changes to RigidBody2D will be overriden by the physics engine
    when running. Change the size of the children collision shapes instead."
    
    Also, set a better default for banana ball random angle.

--- scripts/components/balls/banana_ball.gd
+++ scripts/components/balls/banana_ball.gd
@@ -1,3 +1,4 @@
+@tool
 extends RigidBody2D
 
 ## This is how fast your paddle moves.
@@ -7,12 +8,32 @@ extends RigidBody2D
 @export_range(-180, 180, 1.0, "radians_as_degrees") var initial_direction : float = PI/4
 
 ## The ball will rotate its direction randomly. Zero is no random at all.
-@export_range(0, 180, 1, "radians_as_degrees") var random_angle: float = 0.5
+@export_range(0, 180, 1, "radians_as_degrees") var random_angle: float = PI / 4
+
+## How big is this ball?
+@export_range(0.1, 5.0, 0.1) var size: float = 1.0:
+	set = _set_size
+
 
 const _INITIAL_ANGULAR_VELOCITY = 10.0
+const _INITIAL_RADIUS = 48
+const _INITIAL_HEIGHT = 176
+
+@onready var _shape: CapsuleShape2D = %CollisionShape2D.shape
+@onready var _sprite = %Sprite2D
 
 signal touched_paddle
 
+
+func _set_size(new_size):
+	if not Engine.is_editor_hint():
+		await ready
+	size = new_size
+	if _shape == null or _sprite == null:
+		return
+	_shape.radius = _INITIAL_RADIUS * size
+	_shape.height = _INITIAL_HEIGHT * size
+	_sprite.scale = Vector2(size, size)
 
 func _ready():
 	reset()
