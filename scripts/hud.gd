8aac091227983b73a28622afb1f3d9bdc664183d
Manuel Quiñones <manuel.por.aca@gmail.com>
2024-06-03 22:37:45 -0300

    HUD, export font size

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -5,6 +5,10 @@ extends CanvasLayer
 @export var color: Color = Color.WHITE:
 	set = _set_color
 
+## The size of the score numbers.
+@export_range(50, 500, 0.1) var font_size: float = 200.0:
+	set = _set_font_size
+
 @onready var _score_labels = {
 	Global.Player.LEFT: %PlayerLeftScore,
 	Global.Player.RIGHT: %PlayerRightScore,
@@ -24,6 +28,18 @@ func _on_score_changed():
 	set_players_scores(Global.score[Global.Player.LEFT], Global.score[Global.Player.RIGHT])
 
 
+func _set_font_size(new_font_size: float):
+	font_size = new_font_size
+	if not Engine.is_editor_hint():
+		await ready
+	elif not is_node_ready():
+		return
+	for label: Label in _score_labels.values():
+		label.add_theme_font_size_override("font_size", font_size)
+		# Adjust the pivot to the new size:
+		label.pivot_offset = Vector2(240.0, 176.0 * font_size / 200.0)
+
+
 func _set_color(new_color: Color):
 	if not Engine.is_editor_hint():
 		await ready
@@ -38,9 +54,12 @@ func set_players_scores(left_score: int, right_score: int):
 	var left_text = _score_labels[Global.Player.LEFT].text
 	if str(left_score) != left_text:
 		_score_labels[Global.Player.LEFT].text = str(left_score)
-		DampedOscillator.animate(_score_labels[Global.Player.LEFT], "rotation", 300.0, 7.0, -95.0, 0.5)
+		DampedOscillator.animate(
+			_score_labels[Global.Player.LEFT], "rotation", 300.0, 7.0, -95.0, 0.5
+		)
 	var right_text = _score_labels[Global.Player.RIGHT].text
 	if str(right_score) != right_text:
 		_score_labels[Global.Player.RIGHT].text = str(right_score)
-		DampedOscillator.animate(_score_labels[Global.Player.RIGHT], "rotation", 300.0, 7.0, 95.0, 0.5)
-	
+		DampedOscillator.animate(
+			_score_labels[Global.Player.RIGHT], "rotation", 300.0, 7.0, 95.0, 0.5
+		)
