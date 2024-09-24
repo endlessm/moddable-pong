8552cc3edee4ef7829108c7beb796c081d573af6
Will Thompson <wjt@endlessos.org>
2024-09-24 14:23:19 +0100

    Fix formatting with pre-commit
    https://phabricator.endlessm.com/T35658

--- addons/DampedOscillator/DampedOscillator.gd
+++ addons/DampedOscillator/DampedOscillator.gd
@@ -3,7 +3,10 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_autoload_singleton("DampedOscillator", "res://addons/DampedOscillator/scripts/damped_oscillator.tscn")
+	add_autoload_singleton(
+		"DampedOscillator", "res://addons/DampedOscillator/scripts/damped_oscillator.tscn"
+	)
+
 
 func _exit_tree():
 	remove_autoload_singleton("DampedOscillator")
--- addons/DampedOscillator/scripts/AnimationInstance.gd
+++ addons/DampedOscillator/scripts/AnimationInstance.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 # node values
 var node
 var propertie
@@ -19,37 +18,42 @@ var active = true
 signal started
 signal ended
 
+
 func _physics_process(delta):
 	# Do the math
 	var force = -spring_force * displacement - damp * velocity
 	velocity += force * delta
 	displacement += velocity * delta
-	
+
 	# Check if node is gone
 	var wr = weakref(node)
-	
+
 	if !wr.get_ref():
 		set_physics_process(false)
 		queue_free()
 		return
-	
+
 	# Apply the math
-	
+
 	match typeof(node.get(propertie)):
 		TYPE_FLOAT:
-			node.set(propertie, original_value + displacement*scale_factor)
+			node.set(propertie, original_value + displacement * scale_factor)
 		TYPE_INT:
-			node.set(propertie, original_value + int(displacement*scale_factor))
+			node.set(propertie, original_value + int(displacement * scale_factor))
 		TYPE_VECTOR2:
-			node.set(propertie, original_value + Vector2(displacement,-displacement)*scale_factor )
-	
+			node.set(
+				propertie, original_value + Vector2(displacement, -displacement) * scale_factor
+			)
+
 	if abs(velocity - 0) < 0.001:
-		end() # Bye bye
+		end()  # Bye bye
+
 
 func start():
 	set_physics_process(true)
 	active = true
 	emit_signal("started")
+
 
 func end():
 	emit_signal("ended")
